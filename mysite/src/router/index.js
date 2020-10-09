b02871413b962be4c299a58550a4d6dd93b405b1
unknown <syedmunawwar990@gmail.com>
2020-10-10 00:15:43 +0530

    preload added

--- mysite/src/router/index.js
+++ mysite/src/router/index.js
@@ -3,10 +3,10 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const Home = () => import ('@/components/Home.vue')
-const About = () => import ('@/components/About.vue')
-const Portfolio = () => import ('@/components/Portfolio.vue')
-const Skills = () => import ('@/components/Skills.vue')
+const Home = () => import (/* webpackPrefetch: true */  '@/components/Home.vue')
+const About = () => import (/* webpackPrefetch: true */  '@/components/About.vue')
+const Portfolio = () => import (/* webpackPrefetch: true */  '@/components/Portfolio.vue')
+const Skills = () => import (/* webpackPrefetch: true */  '@/components/Skills.vue')
 
 const router = new Router ({
     // mode : 'history',
